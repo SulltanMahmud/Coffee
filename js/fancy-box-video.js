ec600d6923b2e904e8f40ce46658c59cf5ed8dc0
SultanMahmud <smahmud191155@bscse.uiu.ac.bd>
2024-03-19 13:47:52 +0600

    videosection problem fixed

---
+++ js/fancy-box-video.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  // Initialize Fancybox
+  $("[data-fancybox]").fancybox({
+    // Options
+    youtube: {
+      controls: 0,
+      showinfo: 0,
+    },
+  });
+});
